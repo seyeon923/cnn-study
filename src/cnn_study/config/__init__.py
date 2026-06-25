9a12b12b9d643074f5cb2f9681e3b4d252b71d2a
Kim Se-yeon <tpdussla93@gmail.com>
2026-06-25 11:54:39 +0900

    Remove load_yaml_config from __all__ in config module

--- src/cnn_study/config/__init__.py
+++ src/cnn_study/config/__init__.py
@@ -2,10 +2,8 @@ __all__ = [
     "ComponentConfig",
     "TrainConfig",
     "ExperimentConfig",
-    "load_yaml_config",
 ]
 
 from .component import ComponentConfig
 from .experiment import ExperimentConfig
-from .loader import load_yaml_config
 from .train import TrainConfig
