4b3d13751f571b8923edd7498c60fcd4cf66a9e8
Kim Se-yeon <tpdussla93@gmail.com>
2026-06-26 16:25:30 +0900

    Add DatasetConfig class for dataset configuration management

---
+++ src/cnn_study/config/dataset.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass, field
+from typing import Any
+
+from cnn_study.config.component import ComponentConfig
+
+
+@dataclass(frozen=True, slots=True)
+class DatasetConfig:
+    name: str = "MNIST"
+    root: str = "./data"
+
+    split: str = "train"
+
+    num_classes: int = None
+
+    extra_params: dict[str, Any] = field(default_factory=dict)
+
+    transforms: list[ComponentConfig] = None
+    target_transforms: list[ComponentConfig] = None
